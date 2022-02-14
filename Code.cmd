87b13e32f81d61e47796ceb33df591e22fdb6b46
Ubihazard <ubihazard@users.noreply.github.com>
2022-02-14 18:19:57 +0300

    Add comment

--- Code.cmd
+++ Code.cmd
@@ -1,6 +1,10 @@
 @echo off
 cd /d "%~dp0"
 
+:: Example of Visual Studio Code startup script
+:: for Windows with automatic workspace cleanup
+:: (portable installation is assumed)
+
 node cleanup-workspaces.js -b data
 @call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 start Code.exe %*
