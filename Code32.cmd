759d475c7ef75c5ee9c4d8b17aaf895162118569
Ubihazard <ubihazard@users.noreply.github.com>
2022-10-01 13:47:55 +0300

    Update

--- Code32.cmd
+++ Code32.cmd
@@ -1,5 +1,5 @@
 @echo off
 
 node "%~dp0cleanup-workspaces.js" -b "%~dp0data"
-@call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+@call "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars32.bat"
 start "Visual Studio Code" "%~dp0Code.exe" %*
